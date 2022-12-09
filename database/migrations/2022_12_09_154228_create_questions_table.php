01a132636823749e4206f2dc9bde17f859666dc3
cbmella <cbuguenomella@gmail.com>
2022-12-09 12:47:34 -0300

    add more

---
+++ database/migrations/2022_12_09_154228_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->bigInteger('time')->nullable()->default(5);
+            $table->bigInteger('points')->default(1);
+            $table->foreignId('slide_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
---
+++ database/migrations/2022_12_09_154419_create_progress_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('progress', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('points')->nullable();
+            $table->foreignId('player_id')->constrained();
+            $table->foreignId('answer_id')->nullable()->constrained();
+            $table->foreignId('slide_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('progress');
+    }
+};
